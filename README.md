0ab84cb7f335b1d97e06e2a4cf6cb55ec33d619b
shyang9711 <shyang9711@gmail.com>
2024-10-14 17:48:43 -0700

    backend and frontend

---
+++ README.md
@@ -0,0 +1,17 @@
+# SWE INTERVIEW TEST 
+THIS IS TO TEST YOUR SKILLS TO MEET OUR APP DEVELOPMENT REQUIREMENTS
+
+1. Clone this repo to your local machine.
+2. Read `Assignment Requirements` carefully and follow it.
+4. Please commit your entire code to `your own Github repository` 
+5. On your repo, please add step-by-step instructions on how to launch (for both the Backend and Frontend) to the `README.md`
+6. Share your repo link with us via email.
+
+HAPPY CODING! </br>
+A Round Entertainment
+
+# Instruction
+0. `CD` into backend and frontend folder on terminal and run `npm install` to install all missing dependencies.
+1. `CD` into backend folder on terminal and run `node index.js`.
+2. `CD` into frontend folder on terminal and run `npm start`.
+3. The frontend will fetch json with imageUrl and in order to delete the product `press delete icon` on the top left corner of the product.
--- StarterCode/backend/index.js
+++ StarterCode/backend/index.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 //implement the CORS config
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 //products array
 let products = [
@@ -25,12 +31,19 @@ const fetchImageUrl = () => {
 
 //implement the get api for getting products
 app.get('/api/products', (req, res) => {
+    const imageProducts = products.map(product => ({
+        ...product,
+        imageUrl: fetchImageUrl()
+    }))
+    res.json(imageProducts)
 
 });
 
 //implement the delete api for deleting a product by Id
 app.delete('/api/products/:id', (req, res) => {
-    
+    const id = parseInt(req.params.id);
+    products = products.filter(product => product.id !== id);
+    res.json(products)
 });
 
 app.listen(PORT, () => {
